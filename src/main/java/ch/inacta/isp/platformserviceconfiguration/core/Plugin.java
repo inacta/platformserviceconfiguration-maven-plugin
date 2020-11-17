3476da93daf57d164521a7d5b763dcd29ea2cc5a
Alessandro Bonomo <alessandro.bonomo@inacta.ch>
2020-11-17 16:59:21 +0100

    initialize plugin implementation

---
+++ src/main/java/ch/inacta/isp/platformserviceconfiguration/core/Plugin.java
@@ -0,0 +1,14 @@
+package ch.inacta.isp.platformserviceconfiguration.core;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "rest-request")
+public class Plugin extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException {
+
+    }
+}
