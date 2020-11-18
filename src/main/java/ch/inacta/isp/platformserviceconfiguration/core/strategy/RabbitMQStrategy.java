6e45107c6cc02cc1aef6ad47d3e337a5a91de0fa
Alessandro Bonomo <alessandro.bonomo@inacta.ch>
2020-11-18 10:41:09 +0100

    adds rabbitmq strategy implementation

---
+++ src/main/java/ch/inacta/isp/platformserviceconfiguration/core/strategy/RabbitMQStrategy.java
@@ -0,0 +1,59 @@
+package ch.inacta.isp.platformserviceconfiguration.core.strategy;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
+
+import java.util.Base64;
+import java.util.Map;
+
+import javax.ws.rs.core.MediaType;
+
+import org.apache.maven.plugin.MojoExecutionException;
+
+import ch.inacta.isp.platformserviceconfiguration.core.model.AccessTokenResponse;
+
+/**
+ * Strategy to handle rabbitmq specific authorization.
+ *
+ * @author INACTA AG
+ * @since 1.0.0
+ */
+public class RabbitMQStrategy implements AuthorizationStrategy {
+
+    private static final MediaType REQUEST_TYPE = APPLICATION_JSON_TYPE;
+    private static final MediaType RESPONSE_TYPE = APPLICATION_JSON_TYPE;
+
+    @Override
+    public AccessTokenResponse authorize(final Map<String, String> authParams) throws MojoExecutionException {
+
+        if (!authParams.containsKey("username")) {
+            throw new MojoExecutionException("Tag 'username' has to be defined in authorization!");
+        }
+        if (!authParams.containsKey("password")) {
+            throw new MojoExecutionException("Tag 'password' has to be defined in authorization!");
+        }
+
+        final String username = authParams.get("username");
+        final String password = authParams.get("password");
+
+        final AccessTokenResponse accessTokenResponse = new AccessTokenResponse();
+        accessTokenResponse.setTokenType("Basic");
+
+        final String encodedToken = Base64.getEncoder().encodeToString((username + ":" + password).getBytes(UTF_8));
+        accessTokenResponse.setAccessToken(encodedToken);
+
+        return accessTokenResponse;
+    }
+
+    @Override
+    public MediaType getRequestType() {
+
+        return REQUEST_TYPE;
+    }
+
+    @Override
+    public MediaType getResponseType() {
+
+        return RESPONSE_TYPE;
+    }
+}
