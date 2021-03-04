4ee7673771e0e960de9b3841589c1fddd2ddcf05
Adrian Scherer <adrian.scherer@inacta.ch>
2021-03-04 11:13:39 +0100

    Refactor RabbitMQStrategy

--- src/main/java/ch/inacta/maven/platformserviceconfiguration/core/strategy/RabbitMQStrategy.java
+++ src/main/java/ch/inacta/maven/platformserviceconfiguration/core/strategy/RabbitMQStrategy.java
@@ -5,15 +5,16 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Base64.getEncoder;
 import static javax.ws.rs.client.ClientBuilder.newClient;
 import static javax.ws.rs.client.Entity.form;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import static javax.ws.rs.core.Response.Status.Family.SUCCESSFUL;
+import static org.keycloak.OAuth2Constants.PASSWORD;
+import static org.keycloak.OAuth2Constants.USERNAME;
 
-import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.Form;
 import javax.ws.rs.core.Response;
 
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.logging.Log;
 import org.glassfish.jersey.jackson.JacksonFeature;
 
 import ch.inacta.maven.platformserviceconfiguration.core.Plugin;
@@ -21,18 +22,19 @@ import ch.inacta.maven.platformserviceconfiguration.core.model.AccessTokenRespon
 import ch.inacta.maven.platformserviceconfiguration.core.model.ErrorInfo;
 
 /**
- * Strategy to handle RabbitMQ specific authorization and functionalities.
+ * Strategy to handle RabbitMQ specific configuration tasks.
  *
  * @author Inacta AG
  * @since 1.0.0
  */
 class RabbitMQStrategy {
 
+    private static final String BASIC_TOKEN_TYPE = "Basic";
+    private static final String REST_API_FOR_CREATING_QUEUE = "api/queues/";
     private final Plugin plugin;
-    private final Log logger;
 
     /**
-     * Default constructor
+     * Default constructor.
      *
      * @param plugin
      *            this plugin with all the called parameters
@@ -40,52 +42,48 @@ class RabbitMQStrategy {
     RabbitMQStrategy(final Plugin plugin) {
 
         this.plugin = plugin;
-        this.logger = plugin.getLog();
     }
 
     /**
-     * REST API call to create a queue.
+     * Simple REST call to create a queue.
      */
     void createQueue() throws MojoExecutionException {
 
-        final Invocation.Builder builder = createClientBuilder();
-        executeRequest(builder);
+        if (this.plugin.getResource().isEmpty()) {
+            throw new MojoExecutionException("Tag 'resource' has to be defined with the queue name!");
+        }
+
+        final Response response = executeRequest(REST_API_FOR_CREATING_QUEUE + this.plugin.getResource());
+        processResponse(response);
     }
 
-    private Invocation.Builder createClientBuilder() {
+    private Response executeRequest(final String queueName) {
 
         final AccessTokenResponse accessTokenResponse = getAccessTokenResponse();
-        return newClient().register(JacksonFeature.class).target(this.plugin.getEndpoint()).path(this.plugin.getResource())
+        return newClient().register(JacksonFeature.class).target(this.plugin.getEndpoint()).path(queueName)
                 .request(APPLICATION_JSON_TYPE, APPLICATION_JSON_TYPE)
-                .header("Authorization", accessTokenResponse.getTokenType() + " " + accessTokenResponse.getAccessToken());
+                .header(AUTHORIZATION, accessTokenResponse.getTokenType() + " " + accessTokenResponse.getAccessToken()).put(form(new Form()));
     }
 
     private AccessTokenResponse getAccessTokenResponse() {
 
-        final String username = this.plugin.getAuthorization().get("username");
-        final String password = this.plugin.getAuthorization().get("password");
-
         final AccessTokenResponse accessTokenResponse = new AccessTokenResponse();
-        accessTokenResponse.setTokenType("Basic");
+        accessTokenResponse.setTokenType(BASIC_TOKEN_TYPE);
 
+        final String username = this.plugin.getAuthorization().get(USERNAME);
+        final String password = this.plugin.getAuthorization().get(PASSWORD);
         final String encodedToken = getEncoder().encodeToString((username + ":" + password).getBytes(UTF_8));
         accessTokenResponse.setAccessToken(encodedToken);
 
         return accessTokenResponse;
     }
 
-    private void executeRequest(final Invocation.Builder builder) throws MojoExecutionException {
-
-        final Response response = builder.put(form(new Form()));
-        processResponse(response);
-    }
-
     private void processResponse(final Response response) throws MojoExecutionException {
 
         if (response.getStatusInfo().getFamily() == SUCCESSFUL) {
-            this.logger.info(format("Status: [%d]", response.getStatus()));
+            this.plugin.getLog().info(format("Queue [%s] successfully created, status: [%d]", this.plugin.getResource(), response.getStatus()));
         } else {
-            this.logger.warn(format("Error code: [%d]", response.getStatus()));
+            this.plugin.getLog().warn(format("Error code: [%d]", response.getStatus()));
             final ErrorInfo errorInfo = new ErrorInfo(response.getStatus(), response.getEntity().toString());
             throw new MojoExecutionException(format("Unable to create queue: %s", errorInfo.toString()));
         }
