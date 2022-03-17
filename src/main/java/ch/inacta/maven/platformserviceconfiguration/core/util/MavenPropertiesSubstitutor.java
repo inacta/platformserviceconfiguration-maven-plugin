34796416783046c63d75fc76dc8f78e55afed53b
Michael Brem <m_brem@hispeed.ch>
2022-03-17 16:40:22 +0100

    Update MavenPropertiesSubstitutor.java

--- src/main/java/ch/inacta/maven/platformserviceconfiguration/core/util/MavenPropertiesSubstitutor.java
+++ src/main/java/ch/inacta/maven/platformserviceconfiguration/core/util/MavenPropertiesSubstitutor.java
@@ -14,15 +14,7 @@ public class MavenPropertiesSubstitutor extends StringSubstitutor {
 
     public MavenPropertiesSubstitutor(final Properties properties, String realm) {
 
-        super(key -> {
-            
-            System.out.println(key);
-            System.out.println(realm);
-            System.out.println("tenant".equalsIgnoreCase(key.trim()));
-            System.out.println("VALUE-EVALUATED: " + ("tenant".equalsIgnoreCase(key.trim()) ? realm : properties.getProperty(key)));
-
-            return "tenant".equalsIgnoreCase(key.trim()) ? realm : properties.getProperty(key);
-        });
+        super(key -> "tenant".equalsIgnoreCase(key.trim()) ? realm : properties.getProperty(key));
         
         this.setEnableUndefinedVariableException(false);
         this.setEnableSubstitutionInVariables(true);
